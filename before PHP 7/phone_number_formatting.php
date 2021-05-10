c8a0945d5ba76908d1dc0391a4df31ca56bac031
Hugo Levet <49909831+hugo-levet@users.noreply.github.com>
2021-05-10 16:24:44 +0200

    Add files via upload

---
+++ before PHP 7/phone_number_formatting.php
@@ -0,0 +1,36 @@
+<?php
+function formatPhoneNumber($phoneNumber, $maxPhoneNumber = 1, $separator = ' - ')
+{
+    // get juste number
+    preg_match_all('!\d+!', $phoneNumber, $allPhoneNumbers);
+    $allPhoneNumbers = $allPhoneNumbers[0];
+    $allPhoneNumbers = str_split(implode($allPhoneNumbers));
+    // int var
+    $phoneFormating = '';
+    $leftOverEnd = 10;
+    $leftOverSpace = 2;
+    $nextPhoneNumber = '';
+    // for each digit
+    foreach ($allPhoneNumbers as $phoneNumber) {
+        // do nothing if max is reached
+        if ($maxPhoneNumber != 0) {
+            $nextPhoneNumber .= $phoneNumber;
+            --$leftOverEnd;
+            --$leftOverSpace;
+            if ($leftOverEnd == 0) { // end of phone number
+                if ($phoneFormating != '') {
+                    $phoneFormating .= $separator;
+                }
+                $phoneFormating .= $nextPhoneNumber;
+                $leftOverEnd = 10;
+                $leftOverSpace = 2;
+                --$maxPhoneNumber;
+                $nextPhoneNumber = '';
+            } else if ($leftOverSpace == 0) { // end of pair of number
+                $nextPhoneNumber .= ' ';
+                $leftOverSpace = 2;
+            }
+        }
+    }
+    return $phoneFormating;
+}
